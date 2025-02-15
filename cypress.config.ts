b7a1edbfc7230ea297e48f9d1344c57de809a081
jsonify <phlacin@gmail.com>
2025-02-15 09:48:27 -0800

    Enhance CI workflow with additional testing steps and Cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-	fileServerFolder: 'dist',
-	fixturesFolder: false,
-	projectId: 'etow1b',
-	e2e: {
-		baseUrl: 'http://localhost:4173/',
-		specPattern: 'cypress/e2e/**/*.ts'
-	}
+  fileServerFolder: 'dist',
+  fixturesFolder: false,
+  projectId: 'etow1b',
+  e2e: {
+    baseUrl: 'http://localhost:4173/',
+    specPattern: 'cypress/e2e/**/*.ts',
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
+    screenshotOnRunFailure: true,
+    video: true,
+    videoCompression: 32
+  }
 })
